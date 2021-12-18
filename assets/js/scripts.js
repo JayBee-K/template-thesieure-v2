995bad46fd70ce538c72a1268e4c2bd0b1d3e418
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2021-12-18 10:47:10 +0700

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,8 +19,8 @@ const slideBanner = function () {
 	});
 }
 
-const initRowCard = function () {
-	let elm = $('#form-expandCard');
+const initRowCardChange = function () {
+	let elm = $('#form-expandCard_Change');
 	elm.find('.createRow').click(function () {
 		html = `<div class="row row-10 mb-3 align-items-center">
 									<div class="col-md-3">
@@ -68,7 +68,76 @@ const initRowCard = function () {
 										</a>
 									</div>
 								</div>`;
-		$('#form-expandCard').append(html)
+		$('#form-expandCard_Change').append(html)
+	});
+
+	elm.on('click', '.deleteRow', function () {
+		$(this).closest('.row').remove();
+	});
+}
+
+const initRowCardTopUp = function () {
+	let elm = $('#form-expandCard_TopUp');
+	elm.find('.createRow').click(function () {
+		html = `<div class="row row-10 mb-3 align-items-center">
+								<div class="col-md-2">
+									<div class="form-group">
+										<input type="tel" minlength="10" maxlength="11" class="form-control" id="sdt"
+											   placeholder="Số điện thoại">
+									</div>
+								</div>
+								<div class="col-md-2">
+									<div class="form-group">
+										<select class="form-control" id="nhamang">
+											<option name="-1">--- Nhà mạng ---</option>
+											<option name="">Viettel trả trước</option>
+											<option name="">Viettel trả sau / FTTH / Home</option>
+											<option name="">Vina trả trước</option>
+											<option name="">Vina trả sau</option>
+											<option name="">Mobi trả trước</option>
+											<option name="">Mobi trả sau</option>
+											<option name="">Bắn TKC Viettel</option>
+											<option name="">Bắn TKC Vina</option>
+											<option name="">Bắn TKC Mobi</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-2">
+									<div class="form-group">
+										<select class="form-control" id="price">
+											<option name="-1">--- Mệnh giá ---</option>
+											<option name="10000">10.000 đ</option>
+											<option name="20000">20.000 đ</option>
+											<option name="30000">30.000 đ</option>
+											<option name="50000">50.000 đ</option>
+											<option name="100000">100.000 đ</option>
+											<option name="200000">200.000 đ</option>
+											<option name="300000">300.000 đ</option>
+											<option name="500000">500.000 đ</option>
+											<option name="1000000">1.000.000 đ</option>
+										</select>
+									</div>
+								</div>
+								<div class="col-md-2">
+									<div class="form-group">
+										<input type="text" class="form-control" id="chietkhau" placeholder="Chiết khấu"
+											   readonly>
+									</div>
+								</div>
+								<div class="col-md-3">
+									<div class="form-group">
+										<input type="text" class="form-control" id="ghichu" placeholder="Ghi chú">
+									</div>
+								</div>
+								<div class=" col-md-1 col-3">
+									<a href="javascript:void(0)"
+									   class="createRow btn-create_row btn btn-success btn-sm waves-light waves-effect">
+										<i class="fas fa-plus"></i>
+										Thêm
+									</a>
+								</div>
+							</div>`;
+		$('#form-expandCard_TopUp').append(html)
 	});
 
 	elm.on('click', '.deleteRow', function () {
@@ -81,5 +150,6 @@ $(function () {
 
 	slideBanner();
 
-	initRowCard();
+	initRowCardChange();
+	initRowCardTopUp();
 });
